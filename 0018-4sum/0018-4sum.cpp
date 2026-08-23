23be9a34e485652576cf02f3c0744c3d6309b601
Vaishakh <vaishakh0102@gmail.com>
2026-08-23 15:02:33 +0530

    Time: 23 ms (53.92%), Space: 17.5 MB (24.21%) - LeetHub

---
+++ 0018-4sum/0018-4sum.cpp
@@ -0,0 +1,61 @@
+class Solution {
+public:
+    vector<vector<int>> fourSum(vector<int>& nums, int target) {
+        // better approach-Time Complexity-O(n^3)
+        // int n=nums.size();
+        // set<vector<int>>st;
+        // for(int i=0;i<n;i++){
+        //     set<long long >hashset;
+        //     for(int j=i+1;j<n;j++){
+        //         for(int k=j+1;k<n;k++){
+        //             long long sum= nums[i]+nums[j];
+        //             sum+=nums[k];
+        //             long long fourth= target-sum;
+        //             if(hashset.find(fourth)!=hashset.end()){
+        //                 vector<int>temp={nums[i],nums[j],nums[k],(int)fourth};
+        //                 sort(temp.begin(),temp.end());
+        //                 st.insert(temp);
+        //             }
+        //             hashset.insert(nums[k]);
+        //         }
+               
+        //     }
+
+        // }
+        // vector<vector<int>>ans(st.begin(),st.end());
+        // return ans;
+
+        int n=nums.size();
+        vector<vector<int>>ans;
+        sort(nums.begin(),nums.end());
+        for(int i=0;i<n;i++){
+            if(i>0 && nums[i]==nums[i-1])continue;
+            for(int j=i+1;j<n;j++){
+                if(j>(i+1) && nums[j]==nums[j-1])continue;
+                int k=j+1;
+                int l=n-1;
+                while(k<l){
+                    long long sum=nums[i];
+                    sum+=nums[j];
+                    sum+=nums[k];
+                    sum+=nums[l];
+
+                    
+                    if(sum==target){
+                        ans.push_back({nums[i],nums[j],nums[k],nums[l]});
+                        k++;
+                        l--;
+                        while(k<l&& nums[k]==nums[k-1])k++;
+                        while(k<l&& nums[l]==nums[l+1])l--;
+                        
+                    }
+                    else if(sum<target)k++;
+                    else l--;
+                }
+
+            }
+        }
+        return ans;
+        
+    }
+};
