cebe01982ebfe6b32a9fc969479a60422401039d
Vaishakh <vaishakh0102@gmail.com>
2026-09-07 00:08:18 +0530

    Time: 0 ms (100%), Space: 26.3 MB (38.02%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -10,7 +10,7 @@ public:
         while(low<=high){ 
             int mid=(low+high)/2;
             if(nums[mid]!=nums[mid-1] && nums[mid] !=nums[mid+1])return nums[mid];
-            else if (mid%2==1 && nums[mid]==nums[mid-1]||mid%2==0 && nums[mid]==nums[mid+1]){
+            if (mid%2==1 && nums[mid]==nums[mid-1]||mid%2==0 && nums[mid]==nums[mid+1]){
                 low=mid+1;
             }
             else {
